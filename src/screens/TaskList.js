ddcd2728286bf2d0096340c34a309f81ed1a1ada
Karen Jully <91673156+Kjully01@users.noreply.github.com>
2021-11-22 10:13:52 -0300

    screen
    Tela com imagem

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 
-import todayImage from '../../assets/imgs/today.jpg'
+const todayImage = require('../../assets/imgs/today.jpg')
 
 export default class TaskList extends Component {
     render() {
         return (
-            <View>
-                <ImageBackground source={todayImage}>
+            <View style={styles.container}>
+                <ImageBackground 
+                    source={todayImage}
+                    style={styles.background}>
 
                 </ImageBackground>
-                <Text>TaskList</Text>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3,
+    },
+    taskList: {
+        flex: 7
+    }
+})
