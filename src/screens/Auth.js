2b5b5e683d92f9c01e481bea3716ce8e24b8a9a6
Karen Jully <karenjully.kr15@gmail.com>
2022-01-01 15:57:10 -0300

    validations

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -69,6 +69,19 @@ export default class Auth extends Component {
     }
 
     render(){
+
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+
+        if(this.state.stageNew){
+            validations.push(this.state.name && this.state.name.trim().length >= 3)
+            validations.push(this.state.confirmPassword == this.state.password)
+        }
+
+        //faz validação se todos os elementos do array são verdadeiros. No lugar de fazer um "for"
+        const validForm = validations.reduce((vlTot, vlAt) => vlTot && vlAt)
+
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
@@ -102,8 +115,9 @@ export default class Auth extends Component {
                             secureTextEntry={true}
                             onChangeText={confirmPassword => this.setState({confirmPassword})}/>
                     }
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+                    <TouchableOpacity onPress={this.signinOrSignup}
+                        disabled={!validForm}>
+                        <View style={[styles.button, validForm ? {} : {backgroundColor: '#AAA'}]}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
                             </Text>
